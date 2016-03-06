1394262027612173d3bedef3d0cc96bb4277795a
Max-IM97 <max.matuson@gmail.com>
2016-03-05 23:15:05 -0500

    first commit

--- IOS8SwiftTakePhotoTutorial/ViewController.swift
+++ IOS8SwiftTakePhotoTutorial/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    var imagePicker: UIImagePickerController!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func takePhoto(sender: UIButton) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+            imagePicker.dismissViewControllerAnimated(true, completion: nil)
+            imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
+    }
+    
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
